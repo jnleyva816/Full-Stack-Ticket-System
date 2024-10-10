49fefe66894e03c9001976858b9c6f505bf76889
jnleyva816 <jleyva6439@floridapoly.edu>
2024-10-10 13:52:01 -0400

    initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  redirects: async () => {
+    return [
+      {
+        source: "/logout",
+        destination: "/auth/logout",
+        permanent: true,
+      },
+    ];
+  }
+};
 
 export default nextConfig;
